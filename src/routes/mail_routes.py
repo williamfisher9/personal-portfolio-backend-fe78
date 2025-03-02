ce4a3ea3f0d30a4b62eab57496bd7079efc8e183
williamfisher9 <fisher.a.william@gmail.com>
2025-03-02 04:11:24 -0600

    enhanced logging

--- src/routes/mail_routes.py
+++ src/routes/mail_routes.py
@@ -14,6 +14,6 @@ def send_email():
     logging.info("received a request to send an email")
     logging.info(request.get_json())
     msg = Message(request.get_json()['name'] , sender=os.environ.get('MAIL_USERNAME'), recipients=[os.environ.get('MAIL_USERNAME')])
-    msg.html = f"<div><h1>Sender: {request.get_json()['mail']}</h1><p>{request.get_json()['message']}</p></div>"
+    msg.html = f"<div><p>Sender: {request.get_json()['mail']}</p><p>{request.get_json()['message']}</p></div>"
     mail.send(msg)
     return "Sent", 200
