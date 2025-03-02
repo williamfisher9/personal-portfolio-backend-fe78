89cae33d2557f3b964439229bc698a137aa05efb
williamfisher9 <fisher.a.william@gmail.com>
2025-03-02 04:04:21 -0600

    enhanced logging

--- src/routes/mail_routes.py
+++ src/routes/mail_routes.py
@@ -12,10 +12,8 @@ logging.getLogger(__name__)
 @mail_blueprint.route("/send", methods=['POST'])
 def send_email():
     logging.info("received a request to send an email")
-    logging.info(request.get_json()['name'])
-    logging.info(request.get_json()['mail'])
-    logging.info(request.get_json()['message'])
-    msg = Message(request.get_json()['name'] , sender=request.get_json()['mail'], recipients=[os.environ.get('MAIL_USERNAME')])
-    msg.body = request.get_json()['message']
+    logging.info(request.get_json())
+    msg = Message(request.get_json()['name'] , sender=os.environ.get('MAIL_USERNAME'), recipients=[os.environ.get('MAIL_USERNAME')])
+    msg.body = f"<div><h1>Sender: {request.get_json()['mail']}</h1><p>{request.get_json()['message']}</p></div>"
     mail.send(msg)
     return "Sent", 200
